4fda59df04041480dce515a8f01a14ead12241bf
Kostis Karantias <karantiaskostis@gmail.com>
2014-02-13 16:46:24 +0200

    Add local jQuery

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
-        <script src='http://code.jquery.com/jquery-1.7.1.min.js'></script>
+        <script src='js/jquery-1.7.2.min.js'></script>
         <script src='js/json2.js'></script>
         <script src='js/behavior.js'></script>
         <script src='js/navigation.js'></script>
